b3fb444b544dd26a30cbe4a142d8b170560591ba
Faris Chebib <faris@zaneprep.com>
2016-06-04 12:29:35 -0700

    added emotional states

--- emoji_pet.py
+++ emoji_pet.py
@@ -1,16 +1,38 @@
 from microbit import *
 import music
 
-neutral_face = Image("20002:09090:00000:99999:00000")
+neutral_face = Image("09090:00000:99999:00000:00000")
+grin_face = Image("09090:00000:90009:09990:00000")
+happy_face = Image("09090:00000:99999:90009:09990")
+
+frowning_face = Image("09090:00000:09990:90009:00000")
+sad_face = Image("09090:00000:09990:90009:90009")
+
 thumbs_up = Image("00990:00990:99999:09999:09990")
 
 happy_texts = [""]
 
-music.play(music.POWER_UP)
-display.show(thumbs_up)
+music.play(music.POWER_UP, wait=False)
 # while True:
-#     acx, acy = accelerometer.get_x(), accelerometer.get_y()
-#     if acx > 10:
-#         display.show(Image.HAPPY)
-#     else:
-#         display.show(neutral_face)
+acx, acy = accelerometer.get_x(), accelerometer.get_y()
+
+
+def animate(faces, delay=500):
+    for face in faces:
+        display.show(face)
+        sleep(delay)
+
+
+def get_happy(loops=2):
+    music.play(music.BA_DING, wait=False)
+    for i in range(loops):
+        animate([neutral_face, grin_face, happy_face])
+
+
+def get_sad(loops=2):
+    music.play(music.POWER_DOWN, wait=False)
+    for i in range(loops):
+        animate([neutral_face, frowning_face, sad_face])
+
+while True:
+    get_sad(loops=100)
